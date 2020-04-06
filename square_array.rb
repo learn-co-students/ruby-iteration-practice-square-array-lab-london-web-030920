f1ee1c54b22eb6f45491a007df6eb2424247fe8f
minamalaj <mina.malaj@hotmail.com>
2020-04-06 13:19:37 +0100

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+sqaured_array = [] 
+counter = 0 
+array.each do |number| 
+  sqaured_array << (number * number)
+  counter += 1 
+end 
+sqaured_array
 end
+
+def square_array_with_map(array)
+  array.map do |number| 
+    number * number
+  end 
+end 
